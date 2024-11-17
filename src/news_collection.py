3d08b001e569198a3331ff67594228bb802cfec2
SSJDeathSpawn <adikesavan28@gmail.com>
2024-11-17 23:37:26 +0530

    Get data

---
+++ src/news_collection.py
@@ -0,0 +1,16 @@
+from newsapi import NewsApiClient
+import pandas as pd
+
+api = NewsApiClient(api_key='5eebff47dd2c48b799fa4c1206966735')
+
+global_news = api.get_everything(q='global',
+                                 sources='bbc-news,the-verge',
+                                 domains='bbc.co.uk',
+                                 language='en')
+
+article_list = global_news['articles']
+global_headlines = [x['title'] for x in article_list]
+global_articles = [x['description'] for x in article_list]
+
+data = pd.DataFrame({'headline': global_headlines, 'text': global_articles})
+data.to_csv('news_data.csv', index=False)
