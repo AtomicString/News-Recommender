5a2d0fa5535630bdd2f6f38c1f6a82dc512f3b6b
AtomicString <adikesavan2099@gmail.com>
2024-11-23 19:34:34 +0530

    Final Commit

--- src/test_NER.py
+++ src/test_NER.py
@@ -6,17 +6,26 @@ from transformers import BertTokenizer
 from functools import partial
 
 
-data = pd.read_csv("data/eng_train.txt",
-                   encoding="utf-8", sep=" ", header=None, skip_blank_lines=False)
-data.columns = ["Word", "POS", "IOB tags", "Tag"]
-tag_values = list(set(data["Tag"].values))
+# data = pd.read_csv("data/eng_train.txt",
+#                    encoding="utf-8", sep=" ", header=None, skip_blank_lines=False)
+# data.columns = ["Word", "POS", "IOB tags", "Tag"]
+# tag_values = list(set(data["Tag"].values))
+
+tag_values = ['B-MISC', 'B-ORG', 'I-PER',
+              'B-LOC', 'I-ORG', 'I-MISC', 'I-LOC', 'O', float('nan')]
 tag_values.append("PAD")
+print(tag_values)
 
 
 tokenizer = BertTokenizer.from_pretrained(
     'bert-base-cased', do_lower_case=False)
 
 model = pickle.load(open('models/model.pickle', 'rb'))
+
+seed = 0
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
 
 
 def test_func(test_sentence):
@@ -32,6 +41,7 @@ def test_func(test_sentence):
     # join bpe split tokens
     tokens = tokenizer.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
     new_tokens, new_labels = [], []
+    print(tokens, label_indices)
     for token, label_idx in zip(tokens, label_indices[0]):
         if token.startswith("##"):
             new_tokens[-1] = new_tokens[-1] + token[2:]
@@ -39,6 +49,7 @@ def test_func(test_sentence):
             new_labels.append(tag_values[label_idx])
             new_tokens.append(token)
 
+    print(new_tokens, new_labels)
     for token, label in zip(new_tokens, new_labels):
         # print("{}\t{}".format(label, token))
         if label in ['B-LOC', 'B-MISC', 'B-ORG', 'I-LOC', 'I-MISC', 'I-ORG', 'I-PER']:
